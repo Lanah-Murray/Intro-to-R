13c69b60a6960bd65dad07cf478af0573b7de598
Lanah-Murray <3603013@myuwc.ac.za>
2020-01-28 18:00:51 +0200

    Corrected version of Exercise 4.5 28 January 2020

--- Exercise 4.5.R
+++ Exercise 4.5.R
@@ -9,19 +9,19 @@ library(tidyverse)
 
 laminaria <- read_csv("data/laminaria.csv") 
 
-Exercise_4.5.1 <- laminaria %>% # Tell R which dataframe I are using
+Exercise_4_5_1<- laminaria %>% # Tell R which dataframe I am using
   select(site, total_length) %>%
-  mutate(total_length_half = total_length/2) %>% # Select only specific columns
+  mutate(total_length_half = total_length/2) # Select only specific columns
 
 
-Exercise_4.5_2 <- laminaria %>% 
+Exercise_4_5_2 <- laminaria %>% 
   group_by(site) %>% # Grouping into mentioned dataset
   summarise(mean_bl = mean(blade_length), # Finding the mean for blade length
             min_bl = min(blade_length), # Finding the minimum blade length
             max_bl = max(blade_length),# Finding maximum blade length
              n_bl = n()) # Determining the number of observations
 
-Exercise_4.5.3 <- laminaria %>%  # Tell R which dataframe I am using
+Exercise_4_5_3 <- laminaria %>%  # Tell R which dataframe I am using
   group_by(site,region,stipe_length) %>% #group dataframe by site only
   summarise(max_sm = max(stipe_mass, na.rm = TRUE)) # Summarising largest stipe mass excluding NA's
   
